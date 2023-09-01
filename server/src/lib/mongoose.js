9ea7fca69bbc2cf0da119b50b65e497b2f2650eb
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-09-01 06:39:16 +0100

    change

--- server/src/lib/mongoose.js
+++ server/src/lib/mongoose.js
@@ -1,15 +1,15 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
+// require('dotenv').config();
+// const mongoose = require('mongoose');
 
-mongoose.connection.once('open', () => {
-	console.log('Mongoose connection started....');
-});
+// mongoose.connection.once('open', () => {
+// 	console.log('Mongoose connection started....');
+// });
 
-mongoose.connection.on('error', (e) => {
-	console.log('Mongoose connection failed......' + e);
-});
-async function startMongoose() {
-	await mongoose.connect(process.env.MONGODB_URL);
-}
+// mongoose.connection.on('error', (e) => {
+// 	console.log('Mongoose connection failed......' + e);
+// });
+// async function startMongoose() {
+// 	await mongoose.connect(process.env.MONGODB_URL);
+// }
 
-module.exports = { startMongoose, mongoose };
+// module.exports = { startMongoose, mongoose };
