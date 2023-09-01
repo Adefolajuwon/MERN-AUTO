5da22641f64c3f7b7d66c56c25ddab85a1de7829
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-09-01 06:38:55 +0100

    change

--- server/src/schemas/user.schema.js
+++ server/src/schemas/user.schema.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
 	firstname: {
@@ -19,4 +19,4 @@ const userSchema = new mongoose.Schema({
 	},
 });
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model('User', userSchema);
