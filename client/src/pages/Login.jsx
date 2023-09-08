6571863a15370e4866548807b296d312352e1502
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-09-08 14:51:12 +0100

    removed unwanted code

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
-import { useAuthContext } from "../context/AuthContext";
-import { httpLoginUser } from "../helpers/requests";
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useAuthContext } from '../context/AuthContext';
+import { httpLoginUser } from '../helpers/requests';
 
 const Login = () => {
 	const [userData, setUserData] = useState({});
@@ -19,16 +19,16 @@ const Login = () => {
 	}
 
 	useEffect(() => {
-		if (success === "true" && token) {
-			localStorage.setItem("auth:token", token);
+		if (success === 'true' && token) {
+			localStorage.setItem('auth:token', token);
 			// Redirect
-			window.location.href = "/dashboard";
+			window.location.href = '/dashboard';
 			// navigate("/dashboard");
 
 			return;
 		}
-		if (success === "false" && message) {
-			navigate("/login");
+		if (success === 'false' && message) {
+			navigate('/login');
 		}
 	}, [location]);
 
@@ -37,15 +37,17 @@ const Login = () => {
 		// Validations here
 		const { email, password } = userData;
 		if (!email || !password) {
-			setError("Please fill in all credentials");
+			setError('Please fill in all credentials');
 			return;
 		}
 		if (!validations.validateEmail(email)) {
-			setError("Please enter a valid email address");
+			setError('Please enter a valid email address');
 			return;
 		}
 		if (!validations.validatePassword(password)) {
-			setError("Password must contain at least eight characters, at least one number , both lower and uppercase letters and special characters");
+			setError(
+				'Password must contain at least eight characters, at least one number , both lower and uppercase letters and special characters'
+			);
 			return;
 		}
 		// fetch data
@@ -55,63 +57,81 @@ const Login = () => {
 			setError(response.error);
 			return;
 		}
-		localStorage.setItem("auth:token", response.token);
+		localStorage.setItem('auth:token', response.token);
 		// Redirect
-		window.location.href = "/dashboard";
+		window.location.href = '/dashboard';
 	}
 	return (
-		<div className="w-full h-[100vh] flex justify-center items-center bg-slate-100 sm:bg-white">
+		<div className='w-full h-[100vh] flex justify-center items-center bg-slate-100 sm:bg-white'>
 			{!success && (
-				<div className="w-full sm:w-2/5  h-auto bg-slate-100 rounded-[5px] flex justify-start flex-col items-center py-6">
-					<h3 className="text-medium text-xl mb-4">Log into your Account</h3>
-					<form action="" className="w-4/5 h-auto mt-2" onSubmit={logInAccount}>
-						<article className="w-full flex flex-col  gap-[10px] mb-3">
-							<div className="w-full">
-								<label htmlFor="email" className="block text-[11px] mb-[2px] w-full">
+				<div className='w-full sm:w-2/5  h-auto bg-slate-100 rounded-[5px] flex justify-start flex-col items-center py-6'>
+					<h3 className='text-medium text-xl mb-4'>Log into your Account</h3>
+					<form action='' className='w-4/5 h-auto mt-2' onSubmit={logInAccount}>
+						<article className='w-full flex flex-col  gap-[10px] mb-3'>
+							<div className='w-full'>
+								<label
+									htmlFor='email'
+									className='block text-[11px] mb-[2px] w-full'
+								>
 									Email:
 								</label>
 								<input
-									name="email"
-									type="text"
-									aria-label="First Name"
-									id="firstname"
-									className="w-full px-2 text-[13px]  focus:outline-0 py-1"
-									placeholder="Email address"
+									name='email'
+									type='text'
+									aria-label='First Name'
+									id='firstname'
+									className='w-full px-2 text-[13px]  focus:outline-0 py-1'
+									placeholder='Email address'
 									onChange={handleChange}
 									onFocus={() => setError(null)}
-									value={userData.email || ""}
+									value={userData.email || ''}
 								/>
 							</div>
-							<div className="w-full">
-								<label htmlFor="password" className="block text-[11px] mb-[2px] w-full">
+							<div className='w-full'>
+								<label
+									htmlFor='password'
+									className='block text-[11px] mb-[2px] w-full'
+								>
 									Enter your password:
 								</label>
 								<input
-									name="password"
-									type="password"
-									aria-label="First Name"
-									id="password"
-									className="w-full px-2 text-[13px] focus:outline-0 py-1"
-									placeholder="********"
+									name='password'
+									type='password'
+									aria-label='First Name'
+									id='password'
+									className='w-full px-2 text-[13px] focus:outline-0 py-1'
+									placeholder='********'
 									onChange={handleChange}
 									onFocus={() => setError(null)}
-									value={userData.password || ""}
+									value={userData.password || ''}
 								/>
 							</div>
 						</article>
-						{error && <p className="text-red-500 text-center text-[12px] h-auto">{error}</p>}
-						<button className="w-full h-10 text-[12px] bg-white text-black flex justify-center items-center rounded-[5px] my-3">Log In</button>
+						{error && (
+							<p className='text-red-500 text-center text-[12px] h-auto'>
+								{error}
+							</p>
+						)}
+						<button className='w-full h-10 text-[12px] bg-white text-black flex justify-center items-center rounded-[5px] my-3'>
+							Log In
+						</button>
 					</form>
 
 					<p>Or</p>
-					<a href="https://localhost:8000/api/auth/google" className="w-3/5 h-10 text-[12px] bg-black text-white flex justify-center items-center rounded-[5px] mt-3">
+					<a
+						href='http://localhost:8000/api/auth/google'
+						className='w-3/5 h-10 text-[12px] bg-black text-white flex justify-center items-center rounded-[5px] mt-3'
+					>
 						Sign In With Google
 					</a>
 
-					<div className="h-auto mt-4 flex gap-3">
-						<p className="text-[12px]">
+					<div className='h-auto mt-4 flex gap-3'>
+						<p className='text-[12px]'>
 							Don't have an account?
-							<Link to="/register" className="text-blue-400 ml-2 hover:underline hover:cursor-pointer">
+							<Link
+								to='/register'
+								className='text-blue-400 ml-2 hover:underline hover:cursor-pointer'
+							>
 								Sign Up
 							</Link>
 						</p>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { useAuthContext } from "../context/AuthContext";
-import { httpCreateUser } from "../helpers/requests";
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuthContext } from '../context/AuthContext';
+import { httpCreateUser } from '../helpers/requests';
 
 const Register = () => {
-	const [userData, setUserData] = useState({ provider: "local" });
+	const [userData, setUserData] = useState({ provider: 'local' });
 	const [error, setError] = useState(null);
 	const [loading, setLoading] = useState(true);
 	const { validations } = useAuthContext();
@@ -21,19 +21,21 @@ const Register = () => {
 		// Validations here
 		const { email, firstname, password, confirmpassword } = userData;
 		if (!email || !password || !confirmpassword || !firstname) {
-			setError("Please fill in all credentials");
+			setError('Please fill in all credentials');
 			return;
 		}
 		if (!validations.validateEmail(email)) {
-			setError("Please enter a valid email address");
+			setError('Please enter a valid email address');
 			return;
 		}
 		if (!validations.validatePassword(password)) {
-			setError("Password must contain at least eight characters, at least one number , both lower and uppercase letters and special characters");
+			setError(
+				'Password must contain at least eight characters, at least one number , both lower and uppercase letters and special characters'
+			);
 			return;
 		}
 		if (password !== confirmpassword) {
-			setError("Passwords do not match");
+			setError('Passwords do not match');
 			return;
 		}
 
@@ -44,93 +46,121 @@ const Register = () => {
 			return;
 		}
 		// Redirect
-		navigate("/login");
+		navigate('/login');
 	}
 	return (
-		<div className="w-full h-[100vh] flex justify-center items-center bg-slate-100 sm:bg-white">
-			<div className="w-full sm:w-3/5  h-auto bg-slate-100 rounded-[5px] flex justify-start flex-col items-center py-6">
-				<h3 className="text-medium text-xl mb-4">Register An Account</h3>
-				<form action="" className="w-4/5 h-auto mt-2" onSubmit={registerAccount}>
-					<article className="w-full grid xs:grid-cols-2 grid-cols-1 gap-[10px] mb-3">
-						<div className="w-full">
-							<label htmlFor="firstname" className="block text-[11px] mb-[2px] w-full">
+		<div className='w-full h-[100vh] flex justify-center items-center bg-slate-100 sm:bg-white'>
+			<div className='w-full sm:w-3/5  h-auto bg-slate-100 rounded-[5px] flex justify-start flex-col items-center py-6'>
+				<h3 className='text-medium text-xl mb-4'>Register An Account</h3>
+				<form
+					action=''
+					className='w-4/5 h-auto mt-2'
+					onSubmit={registerAccount}
+				>
+					<article className='w-full grid xs:grid-cols-2 grid-cols-1 gap-[10px] mb-3'>
+						<div className='w-full'>
+							<label
+								htmlFor='firstname'
+								className='block text-[11px] mb-[2px] w-full'
+							>
 								First name:
 							</label>
 							<input
-								name="firstname"
-								type="text"
-								aria-label="First Name"
-								id="firstname"
-								className="w-full px-2 text-[13px]  focus:outline-0 py-1"
-								placeholder="First Name"
+								name='firstname'
+								type='text'
+								aria-label='First Name'
+								id='firstname'
+								className='w-full px-2 text-[13px]  focus:outline-0 py-1'
+								placeholder='First Name'
 								onChange={handleChange}
 								onFocus={() => setError(null)}
-								value={userData.firstname || ""}
+								value={userData.firstname || ''}
 							/>
 						</div>
-						<div className="w-full">
-							<label htmlFor="email" className="block text-[11px] mb-[2px] w-full">
+						<div className='w-full'>
+							<label
+								htmlFor='email'
+								className='block text-[11px] mb-[2px] w-full'
+							>
 								Email:
 							</label>
 
 							<input
-								name="email"
-								type="text"
-								aria-label="Email Address"
-								id="email"
-								className="w-full px-2 text-[13px]  focus:outline-0 py-1"
-								placeholder="Email Address"
+								name='email'
+								type='text'
+								aria-label='Email Address'
+								id='email'
+								className='w-full px-2 text-[13px]  focus:outline-0 py-1'
+								placeholder='Email Address'
 								onChange={handleChange}
 								onFocus={() => setError(null)}
-								value={userData.email || ""}
+								value={userData.email || ''}
 							/>
 						</div>
-						<div className="w-full">
-							<label htmlFor="password" className="block text-[11px] mb-[2px] w-full">
+						<div className='w-full'>
+							<label
+								htmlFor='password'
+								className='block text-[11px] mb-[2px] w-full'
+							>
 								Password:
 							</label>
 							<input
-								name="password"
-								type="password"
-								aria-label="First Name"
-								id="password"
-								className="w-full px-2 text-[13px] focus:outline-0 py-1"
-								placeholder="********"
+								name='password'
+								type='password'
+								aria-label='First Name'
+								id='password'
+								className='w-full px-2 text-[13px] focus:outline-0 py-1'
+								placeholder='********'
 								onChange={handleChange}
 								onFocus={() => setError(null)}
-								value={userData.password || ""}
+								value={userData.password || ''}
 							/>
 						</div>
-						<div className="w-full">
-							<label htmlFor="confirmpassword" className="block text-[11px] mb-[2px] w-full">
+						<div className='w-full'>
+							<label
+								htmlFor='confirmpassword'
+								className='block text-[11px] mb-[2px] w-full'
+							>
 								Confirm Password:
 							</label>
 							<input
-								name="confirmpassword"
-								type="password"
-								aria-label="First Name"
-								id="confirmpassword"
-								className="w-full px-2 text-[13px]  focus:outline-0 py-1"
-								placeholder="********"
+								name='confirmpassword'
+								type='password'
+								aria-label='First Name'
+								id='confirmpassword'
+								className='w-full px-2 text-[13px]  focus:outline-0 py-1'
+								placeholder='********'
 								onChange={handleChange}
 								onFocus={() => setError(null)}
-								value={userData.confirmpassword || ""}
+								value={userData.confirmpassword || ''}
 							/>
 						</div>
 					</article>
-					{error && <p className="text-red-500 text-center text-[12px] h-auto">{error}</p>}
-					<button className="w-full h-10 text-[12px] bg-white text-black flex justify-center items-center rounded-[5px] my-3">Register</button>
+					{error && (
+						<p className='text-red-500 text-center text-[12px] h-auto'>
+							{error}
+						</p>
+					)}
+					<button className='w-full h-10 text-[12px] bg-white text-black flex justify-center items-center rounded-[5px] my-3'>
+						Register
+					</button>
 				</form>
 
 				<p>Or</p>
-				<a href="https://localhost:8000/api/auth/google" className="w-3/5 h-10 text-[12px] bg-black text-white flex justify-center items-center rounded-[5px] mt-3">
+				<a
+					href='http://localhost:8000/api/auth/google'
+					className='w-3/5 h-10 text-[12px] bg-black text-white flex justify-center items-center rounded-[5px] mt-3'
+				>
 					Sign Up With Google
 				</a>
 
-				<div className="h-auto mt-4 flex gap-3">
-					<p className="text-[12px]">
+				<div className='h-auto mt-4 flex gap-3'>
+					<p className='text-[12px]'>
 						Already have an account?
-						<Link to="/login" className="text-blue-400 ml-2 hover:underline hover:cursor-pointer">
+						<Link
+							to='/login'
+							className='text-blue-400 ml-2 hover:underline hover:cursor-pointer'
+						>
 							Login
 						</Link>
 					</p>
